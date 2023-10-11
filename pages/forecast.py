664fd110b12a28b102b527353de9825fec30689e
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-11 19:06:21 +0700

    Update forecast.py

--- pages/forecast.py
+++ pages/forecast.py
@@ -1,3 +1,6 @@
+import streamlit as st
+
+
 add_selectbox=st.sidebar.selectbox(
     "What Algorithm do you want to use?",
     ("Linear regression", "Logistic regression", "ARIMA","GRU","LSTM", "RNN","SSA")
