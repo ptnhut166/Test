2caa2bdfff383355c0b109794437f2626017c60a
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-10-13 16:42:17 +0700

    Update forecast.py

--- pages/forecast.py
+++ pages/forecast.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from main import selected_stock
+import yfinance as yf
 
 add_selectbox=st.sidebar.selectbox(
     "What Algorithm do you want to use?",
     ("Linear regression", "Logistic regression", "ARIMA","GRU","LSTM", "RNN","SSA")
 )
 
-st.subheader("""Daily **volume** for """ + selected_stock)
+stock_data = yf.Ticker(selected_stock)
+stock_df = stock_data.history(period='1d', start='2020-01-01', end=None)
